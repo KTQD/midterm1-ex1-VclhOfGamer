17733aab7c5e41fc94a7de1105556d396c940105
VclhOfGamer <157661369+VclhOfGamer@users.noreply.github.com>
2024-03-14 07:59:42 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,22 @@
-public class MP3 {
+public class MP3 extends Item {
+    private String duration;
+    public MP3(String name, String description, String ID, float price) {
+        super(name, description, ID, price);
+    }
+    public String getDuration() {
+        return duration;
+    }
+
+    public void setDuration(String duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public void showInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Description: " + getDescription());
+        System.out.println("ID: " + ID);
+        System.out.println("Price: " + price);
+        System.out.println("Duration: " + duration + " seconds");
+    }
 }
