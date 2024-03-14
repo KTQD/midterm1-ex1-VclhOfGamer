23564b9da7653f9748e52495332fa5062c69f0ec
VclhOfGamer <157661369+VclhOfGamer@users.noreply.github.com>
2024-03-14 07:40:23 +0700

    Update Item.java

--- src/Item.java
+++ src/Item.java
@@ -1,2 +1,19 @@
-public class Item {
+abstract class Item {
+    public String name;
+    private String description;
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    protected String ID;
+    float price;
+    void showinfo(){};
+    public Item(String name, String description, String ID, float price){
+        this.name = name;
+        this.description = description;
+        this.ID = ID;
+        this.price = price;
+    }
 }
