7b3e4c845ad871790cd43bbb35a67130fc1b891d
VclhOfGamer <157661369+VclhOfGamer@users.noreply.github.com>
2024-03-14 08:00:42 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        MP3 mp3 = new MP3("That tinh", "From trinh dinh quang", "12004", 59.99f);
+        Book book = new Book("Java", "Ngon ngu lap trinh", "67890", 39.99f, "Dat", 100, "Programming");
 
-        MP3 mp3;
+        System.out.println("Thong tin mp3:");
         mp3.showInfo();
 
-        Book book;
+        System.out.println("\nthong tin book:");
         book.showInfo();
-
     }
 }
